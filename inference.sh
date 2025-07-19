a980b8bfced9ce9b59ef20f2aa40d915a0eff4a0
Xiaochen Zhu <xz479@cl.cam.ac.uk>
2025-07-20 00:30:16 +0100

    initial commit

--- inference.sh
+++ inference.sh
@@ -1,3 +1,2 @@
 
-python train_segment_diffusion.py --eval_test --resume_dir /home/xz479/rds/hpc-work/sentence-diffusion/saved_diff_models/full_roc_utt_jsonl/2024-11-14_01-32-39 \
---sampling_timesteps 250 
+python train_segment_diffusion.py --eval_test --resume_dir your_save_path --sampling_timesteps 250 
